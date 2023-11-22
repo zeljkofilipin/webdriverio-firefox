7352bde87eb7aaaa3e46f074f2d6c5b4c9a1715f
Željko Filipin <zeljko.filipin@gmail.com>
2023-11-22 15:43:15 +0100

    Downgrade to wdio v7

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,5 +1,3 @@
-const { expect, browser, $ } = require('@wdio/globals')
-
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await browser.url(`https://the-internet.herokuapp.com/login`)
